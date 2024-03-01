1c54cd8101bff7f8b6b6deac94daa9561ab99449
popooui67 <upookluy@gmail.com>
2024-03-01 21:24:03 +0100

    sdsdd

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,4 +1,7 @@
-export default function  Profile () {
+import { CreateUser } from "@/lib/action/user.action"
+
+export default async function  Profile () {
+    const user = await CreateUser({email :"zezezezez@sdsds", username:"sdsds",firstName:"sdsdsds"})
     return(
         <></>
     )
